a6bf03adaa449e1d6450de9294e16d59edadc012
Victor Gregory <vicgregor@pm.me>
2025-11-04 05:55:16 +0000

    add file

---
+++ nix-configuration.nix
@@ -0,0 +1,243 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+let
+    sources = import /home/psygreg/.nix/sources.nix;
+    lanzaboote = import sources.lanzaboote;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      lanzaboote.nixosModules.lanzaboote
+    ];
+
+  # Bootloader -- modified for lanzaboote
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelParams = [ 
+	"quiet"
+	"splash"
+	"kernel.split_lock_mitigate=0"
+	"i915.force_probe=!e20b"
+	"xe.force_probe=e20b"
+  ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "intl";
+  };
+
+  # Configure console keymap
+  console.keyMap = "us-acentos";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # additional hardware
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+        intel-compute-runtime
+	driversi686Linux.intel-media-driver
+        intel-vaapi-driver
+        intel-graphics-compiler
+    ];
+  };
+  
+  hardware.openrazer.enable = true;
+  zramSwap.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.psygreg = {
+    isNormalUser = true;
+    description = "Victor";
+    extraGroups = [ "networkmanager" "wheel" "podman" "openrazer" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  # enable starship
+  programs.starship.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # enable flatpaks
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	# gnome extensions and stuff
+	gnomeExtensions.arcmenu
+	gnomeExtensions.appindicator
+	gnomeExtensions.dash-to-panel
+	gnomeExtensions.caffeine
+	gnomeExtensions.clipboard-indicator
+	refine
+	# utilities
+	podman-compose
+	distrobox
+	boxbuddy
+	host-spawn
+	addwater
+	starship
+	git
+	lshw
+	appimage-run
+	pciutils
+	openrazer-daemon
+	polychromatic
+	niv
+	sbctl
+	# apps
+	mission-center
+	discord
+	protonplus
+	audacity
+	gimp3
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ]; 
+
+  virtualisation = {
+        containers.enable = true;
+        podman = {
+                enable = true;
+                dockerCompat = true;
+                defaultNetwork.settings.dns_enabled = true; # Required for cont>
+        };
+  };
+
+  # nix management automations
+  nix.settings.auto-optimise-store = true;
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+  system.autoUpgrade.enable = true;
+
+  # environment variable fixes
+  environment.sessionVariables = {
+	VDPAU_DRIVER = "va_gl";
+	GSK_RENDERER = "gl";
+	MESA_SHADER_CACHE_MAX_SIZE = "12G";
+  };
+
+  # other performance stuff
+  services.preload.enable = true;
+
+  # earlyOOM setup
+  services.earlyoom = {
+      enable = true;
+      freeSwapThreshold = 2;
+      freeMemThreshold = 2;
+      extraArgs = [
+          "-g" "--avoid '^(X|plasma.*|konsole|kwin|wayland|gnome*)$'"
+      ];
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
