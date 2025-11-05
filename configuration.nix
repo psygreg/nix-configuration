3c1d29e4c9528fb66d6778f7fc1d3309a79df000
Victor Gregory <vicgregor@pm.me>
2025-11-05 05:59:14 +0000

    update hardware.graphics

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,7 @@ in
   # additional hardware
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
         intel-compute-runtime
 	driversi686Linux.intel-media-driver
