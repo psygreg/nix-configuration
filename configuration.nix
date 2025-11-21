9e56bf236722fcfca34415b44ff662a38a7e9754
Victor Gregory <vicgregor@pm.me>
2025-11-21 21:07:26 +0000

    updated configuration with performance patches and Plasma 6

--- configuration.nix
+++ configuration.nix
@@ -31,14 +31,19 @@ in
     };
 
   # Use latest kernel.
-    kernelPackages = pkgs.linuxPackages;
-    kernelParams = [ 
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "tcp_bbr" ];
+    kernelParams = [
 	    "quiet"
 	    "splash"
+	    "preempt=full"
     ];
     kernel.sysctl = {
       "kernel.split_lock_mitigate" = 0;
       "kernel.nmi_watchdog" = 0;
+      "net.core.netdev_max_backlog" = 4096;
+      "fs.file-max" = 2097152;
+      "net.ipv4.tcp_congestion_control" = "bbr";
     };
   };
 
@@ -80,9 +85,12 @@ in
   services = { 
     xserver.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
+    # Enable the Plasma Desktop Environment.
+    displayManager.sddm = { 
+	    enable = true;
+	    wayland.enable = true;
+    };
+    desktopManager.plasma6.enable = true;
 
     # Configure keymap in X11
     xserver.xkb = {
@@ -141,11 +149,50 @@ in
         "me.proton.Mail"
         "org.prismlauncher.PrismLauncher"
 	      "com.mattjakeman.ExtensionManager"
+	      "org.upscayl.Upscayl"
+	      "org.chromium.Chromium"
+	      "org.audacityteam.Audacity"
       ];
       update.auto = {
         enable = true;
         onCalendar = "daily";
       };
+    };
+    
+    # udev rules
+    udev = {
+	    enable = true;
+	    extraRules = ''
+		    ACTION=="add", SUBSYSTEM=="sound", KERNEL=="card*", DRIVERS=="snd_hda_intel", TEST!="/run/udev/snd-hda-intel-powersave", \
+    			RUN+="${pkgs.bash}/bin/bash -c 'touch /run/udev/snd-hda-intel-powersave; \
+        			[[ $$(cat /sys/class/power_supply/BAT0/status 2>/dev/null) != \"Discharging\" ]] && \
+        			echo $$(cat /sys/module/snd_hda_intel/parameters/power_save) > /run/udev/snd-hda-intel-powersave && \
+        			echo 0 > /sys/module/snd_hda_intel/parameters/power_save'"
+
+		    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", TEST=="/sys/module/snd_hda_intel", \
+    			RUN+="${pkgs.bash}/bin/bash -c 'echo $$(cat /run/udev/snd-hda-intel-powersave 2>/dev/null || \
+        			echo 10) > /sys/module/snd_hda_intel/parameters/power_save'"
+
+		    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", TEST=="/sys/module/snd_hda_intel", \
+    			RUN+="${pkgs.bash}/bin/bash -c '[[ $$(cat /sys/module/snd_hda_intel/parameters/power_save) != 0 ]] && \
+        			echo $$(cat /sys/module/snd_hda_intel/parameters/power_save) > /run/udev/snd-hda-intel-powersave; \
+        			echo 0 > /sys/module/snd_hda_intel/parameters/power_save'"
+
+		    KERNEL=="rtc0", GROUP="audio"
+		    KERNEL=="hpet", GROUP="audio"
+
+		    DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+
+		    # HDD
+		    ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", \
+    			ATTR{queue/scheduler}="bfq"
+		    # SSD
+		    ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", \
+    			ATTR{queue/scheduler}="mq-deadline"
+		    # NVMe SSD
+		    ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", \
+    		  ATTR{queue/scheduler}="none"
+	    '';
     };
   };
 
@@ -180,10 +227,10 @@ in
         rocblas
         hipblas
         clr
-	rocm-runtime
-	rocm-device-libs
-	rocminfo
-	rocm-smi
+	      rocm-runtime
+	      rocm-device-libs
+	      rocminfo
+	      rocm-smi
       ];
     };
   in [
@@ -216,6 +263,11 @@ in
   	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
   };
+  
+  xdg.portal = with pkgs; {
+	  enable = true;
+	  extraPortals = [ xdg-desktop-portal-gtk ];
+  };
 
   # enable flathub
   systemd.services.flatpak-repo = {
@@ -230,7 +282,7 @@ in
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # gnome stuff
-	  refine
+	  kdePackages.partitionmanager
 	  tela-icon-theme
 	  ffmpegthumbnailer
 	  # utilities
@@ -252,9 +304,10 @@ in
 	  # apps
 	  mission-center
 	  protonplus
-	  audacity
 	  gimp3
 	  heroic
+	  faugus-launcher
+	  protontricks
 	  vintagestory
 	  # OBS setup
 	  obs-studio
