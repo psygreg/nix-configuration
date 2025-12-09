1cb803631c6920d7710e7d2b9886a809e2e0120a
psygreg <psygreg@pm.me>
2025-12-09 16:22:51 -0300

    add dependencies for LT workflow on distrobox

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # ubuntu image: quay.io/toolbx/ubuntu-toolbox
 distrobox enter ubuntu -- bash -c "
     sudo apt update && sudo apt upgrade -y \
-    sudo apt install -y devscripts dput curl build-essential whiptail libdw-dev gcc libncurses-dev curl gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf make rustc bc rsync python-is-python3 perl gettext cpio pahole debhelper dwarves zstd \
+    sudo apt install -y devscripts dput curl build-essential whiptail libdw-dev gcc libncurses-dev curl gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf make rustc bc rsync python-is-python3 perl gettext cpio pahole debhelper dwarves zstd libgirepository-2.0-dev python3-dev libcairo-dev cmake pkg-config meson ninja-build \
     exit 0"
 # fedora image: quay.io/fedora/fedora-toolbox
 distrobox enter fedora -- bash -c "
